1b3093f52e0012efb58bdcc36fd37010d111db59
Thiago Suzuki <thiagoikenaga@gmail.com>
2021-09-06 10:58:52 -0300

    Basic static components

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a className= "text-decoration-none" href="https://github.com/thiago-suzuki" target="_blank" rel="noreferrer">Thiago Suzuki</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                Evento promovido pela escola DevSuperior: <a className= "text-decoration-none" href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
